0434592100f338decb7b0d806569a81716f6e05b
priyasharma0437-cmyk <priya.sharma0437@gmail.com>
2026-02-02 21:30:49 -0800

    Create module3.cpp

---
+++ Module 3 /module3.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+using namespace std; 
+int main() {
+string itemName;
+int quantity;
+float cost;
+
+cout << "Enter item name.";
+cin >> itemName;
+cout << "Enter quantity: ";
+cin >> quantity;
+cout << "Enter cost:";
+cin >> cost; 
+
+cout << "Total cost for" << itemName << "$" << (quantity * cost) << endl;
+return 0;
+}
