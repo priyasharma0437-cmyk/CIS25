bd10ce65b5d61e8d60e5075ddbb3af8b05320414
priyasharma0437-cmyk <priya.sharma0437@gmail.com>
2026-02-09 23:13:54 -0800

    Update assignment4.cpp

--- Assignment 4/assignment4.cpp
+++ Assignment 4/assignment4.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+// Missing commas 
+    float n1, n2, n3;
+
+    cout << "Enter three numbers: ";
+    cin >> n1 >> n2 >> n3;
+// replace == with >= 
+    if (n1 >= n2 && n1 >= n3) {
+        cout << "Largest number: " << n1;
+    }
+//Need to get if n2 is the largest not n1, we already checked that  
+    if (n2 >= n1 && n2 >= n3) {
+        cout << "Largest number: " << n2;
+    }
+// Need to get if n3 is the largest not n2, we already checked that  
+    if (n3 >= n1 && n3 >= n2) {
+        cout << "Largest number: " << n3;
+    }
+
+    return 0;
+}
