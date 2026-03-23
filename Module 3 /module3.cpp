c75c143e7fa77a6852ab219b51d792791e3216fc
priyasharma0437-cmyk <priya.sharma0437@gmail.com>
2026-03-23 02:47:05 -0700

    Update module3pt1.cpp

--- Module 3 /module3.cpp
+++ Module 3 /module3.cpp
@@ -1,17 +1,55 @@
 #include <iostream> 
-using namespace std; 
-int main() {
-string itemName;
-int quantity;
-float cost;
 
-cout << "Enter item name.";
-cin >> itemName;
-cout << "Enter quantity: ";
-cin >> quantity;
-cout << "Enter cost:";
-cin >> cost; 
+using namespace std;
 
-cout << "Total cost for" << itemName << "$" << (quantity * cost) << endl;
-return 0;
+int main() 
+{
+    double balance = 0;
+    double amount = 0; 
+    int choice = 0; 
+    bool quit = false; 
+
+    do 
+    {
+        cout << "1) Deposit" << endl; 
+        cout << "2) Withdraw" << endl;
+        cout << "3) Print Balance" << endl;
+        cout << "4) Print Balance and Quit" << endl; 
+        cout << "5) Quit" << endl; 
+        cout << "Enter Choice: "; 
+        cin >> choice; 
+        
+        switch (choice)
+        { 
+            case 1: 
+                cout << "Enter Amount:" ; 
+                cin >> amount; 
+                balance += amount; 
+                break;
+            case 2: 
+                cout << "Enter Amount:"; 
+                cin >> amount;
+                balance -= amount; 
+                break; 
+            case 3: 
+                cout << "Balance: " << balance << endl;
+                break; 
+
+            case 4: 
+                cout << "Balance: " << balance << endl; 
+                cout << "Goodbye!" << endl;
+                quit = true;
+                break;
+            case 5: 
+                cout << "Goodbye!" << endl; 
+                quit = true;
+                break; 
+            default: 
+                cout << "Invalid option entered!" << endl; 
+        }
+
+    } while (!quit);
+
+
+    return 0;
 }
